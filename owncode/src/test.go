b5c2687c4bde5df8fc77f3a06e539b3e5c26e3fa
michaeleichelbeck <michael.a.eichelbeck@capgemini.com>
2017-09-19 11:31:41 +0200

    .

--- owncode/src/test.go
+++ owncode/src/test.go
@@ -76,7 +76,7 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	// Handle different functions
 	if function == "SetAsset" {
 		return t.SetAsset(stub, args)
-	} else if function == "ContainerHistorian" {
+	} else if function == "GenericWrite" {
 		return t.write(stub, args)
 	} else if function == "UpdateOrderStatus" {
 		return t.UpdateOrderStatus(stub, args)
